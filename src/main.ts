dbe216d349d1ed008462cf65cc16ce61a37fdc41
Lance Vo <lvo@vinavo.us>
2019-02-12 12:10:41 -0600

    replace rssreader with rss feader

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 5678;
+  await app.listen(port);
+  console.log('* started rss reader')
 }
 bootstrap();
--- src/rssreader/rssreader.controller.ts
+++ src/rssreader/rssreader.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { url } from 'inspector';
+const Parser = require('rss-parser');
+const customFields = {
+  headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36' },
+  customFields: {
+    item: ['description'],
+  }
+}
+const parser = new Parser(customFields);
+const getUnixTime = dt => Math.floor(new Date(dt).getTime() / 1000);
 
 @Controller('rssreader')
-export class RssreaderController {}
+export class RssreaderController {
+  @Post()
+  async readFeed(@Body() params: any) {
+    try {
+      let feed = await parser.parseURL(params.url);
+      // return feed;
+      let result = {
+        total: feed.items.length,
+        feed: {
+          entries: feed.items.map(item => ({
+            title: item.title,
+            author: item.author,
+            permalinkUrl: item.link,
+            categories: item.categories,
+            published: getUnixTime(item.pubDate),
+            content: item['content:encoded'],
+            summary: item.description
+          }))
+        }
+      };
+      return result;
+    }
+
+    catch (error) {
+      return error;
+    }
+
+
+
+
+  }
+}
